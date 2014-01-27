b0d69ff959bfb4a3cff056a9ec98741047f79d9f
Toomore Chiang <toomore0929@gmail.com>
2014-01-28 03:18:43 +0800

    To be func.

--- nexmo.py
+++ nexmo.py
@@ -2,17 +2,20 @@
 import requests
 import setting
 
-text = u'這是一封測試簡訊 This a test SMS.'
-endpoint = 'https://rest.nexmo.com/sms/json'
 
-data = {
-    'api_key': setting.api_key,
-    'api_secret': setting.api_secret,
-    'from': setting.msg_from,
-    'to': setting.msg_to,
-    'type': 'unicode',
-    'text': text*2 + str(len(text*2)),
-}
+ENDPOINT = 'https://rest.nexmo.com/sms/json'
 
-r = requests.post(endpoint, data=data)
-print r.text
+def nexmo(data):
+    return requests.post(ENDPOINT, data=data).text
+
+if __name__ == '__main__':
+    text = u'這是一封測試簡訊 This a test SMS.'
+    data = {
+        'api_key': setting.api_key,
+        'api_secret': setting.api_secret,
+        'from': setting.msg_from,
+        'to': setting.msg_to,
+        'type': 'unicode',
+        'text': text*2 + str(len(text*2)),
+    }
+    print nexmo(data)
