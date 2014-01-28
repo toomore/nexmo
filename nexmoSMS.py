4304447e269806a5245ba96a9b1a1cb823780180
Toomore Chiang <toomore0929@gmail.com>
2014-01-28 19:36:04 +0800

    Add description in nexmoSMS.

--- nexmoSMS.py
+++ nexmoSMS.py
@@ -6,7 +6,12 @@ import setting
 ENDPOINT = 'https://rest.nexmo.com/sms/json'
 
 def nexmoSMS(data):
-    return requests.post(ENDPOINT, data=data).text
+    '''
+    :param dict data: send data
+    :rtype: dict
+    :returns: {"message-count":"1","messages":[{"to":"886972xxxxxx","message-id":"0500000004E3EC35","status":"0","remaining-balance":"1.67960000","message-price":"0.01100000","network":"46692"}]}
+    '''
+    return requests.post(ENDPOINT, data=data).json()
 
 if __name__ == '__main__':
     text = u'這是一封測試簡訊 This a test SMS.'
@@ -15,7 +20,11 @@ if __name__ == '__main__':
         'api_secret': setting.api_secret,
         'from': setting.msg_from,
         'to': setting.msg_to,
+        'client-ref': setting.client_ref,
         'type': 'unicode',
         'text': text*2 + str(len(text*2)),
     }
-    print nexmoSMS(data)
+    r = nexmoSMS(data)
+    print r
+    print type(r)
+    print r['messages']
