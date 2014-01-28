9a4021304731687786d838776f73a2779e6da7fc
Toomore Chiang <toomore0929@gmail.com>
2014-01-29 01:25:59 +0800

    Add tts-prompt.

--- nexmoVoice.py
+++ nexmoVoice.py
@@ -3,7 +3,8 @@ import requests
 import setting
 
 
-ENDPOINT = 'https://rest.nexmo.com/tts/json'
+#ENDPOINT = 'https://rest.nexmo.com/tts/json'
+ENDPOINT = 'https://rest.nexmo.com/tts-prompt/json'
 
 def nexmoVoice(data):
     return requests.post(ENDPOINT, data=data).text
@@ -16,6 +17,8 @@ if __name__ == '__main__':
         'from': setting.msg_from,
         'to': setting.msg_to,
         'lg': 'zh-cn',
+        'max_digits': 7, #tts-prompt
+        'bye_text': 'Thanks~~', #tts-prompt
         'text': text*2 + str(len(text*2)),
     }
     print nexmoVoice(data)
